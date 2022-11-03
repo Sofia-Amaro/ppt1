cc3c7cadaec44fa2374a493ae0f8977d9e5f19d7
Sofia-Amaro <112632550+Sofia-Amaro@users.noreply.github.com>
2022-11-03 10:08:56 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,31 @@
 input.onButtonPressed(Button.A, function () {
     if (valor == 1) {
         basic.showIcon(IconNames.Target)
-        basic.showNumber(valor)
     } else if (valor == 2) {
         basic.showIcon(IconNames.Square)
-        basic.showNumber(valor)
     } else {
         basic.showIcon(IconNames.Scissors)
-        basic.showNumber(valor)
     }
     valor += 1
     if (valor > 3) {
         valor = 1
     }
 })
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.Target)
+        basic.showIcon(IconNames.Square)
+        basic.showIcon(IconNames.Scissors)
+    }
+    Azar = randint(1, 3)
+    if (Azar == 0) {
+        basic.showIcon(IconNames.Target)
+    } else if (Azar == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+let Azar = 0
 let valor = 0
 valor = 0
